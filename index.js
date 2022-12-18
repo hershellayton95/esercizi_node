a2ad2b60eaea881106f23cefa369222074bcf22a
Filippo Di Marco <filidm5795@gmail.com>
2022-12-18 15:23:03 +0100

    create exercise 14

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-Use console methods to complete the challenges in this script:
-
 // Challenge 1:
 //
 // Use 2 different techniques to output the value of this variable with
 // a label, so we can easily identify it in the script output.
 
 const surprisingFact = "The bumblebee bat is the world's smallest mammal";
+
+console.log("Bumblebee:", surprisingFact);
+console.log(`Bumblebee: ${surprisingFact}`);
+console.log("Bumblebee: %s", surprisingFact);
 
 // Challenge 2:
 //
@@ -33,16 +35,23 @@ const familyTree = [
   },
 ];
 
+console.dir(familyTree, {depth: null});
+console.log(JSON.stringify(familyTree, null, 2));
+
+
 // Challenge 3:
 //
 // Output a count value every time the importantTask function is called.
 // Reset the count value after 4 function calls.
 
-function importantTask() {}
+function importantTask() {
+    console.count("n")
+}
 
 importantTask();
 importantTask();
 importantTask();
 importantTask();
+console.countReset("n")
 importantTask();
 importantTask();
--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-import app from "./app";
-
-const port = process.env.PORT;
-
-app.listen(port, () => {
-    console.log(`[server]: the server started in http://localhost:${port}`);
-});
